38731569f76e7ea652453a086fd397c5ece95f97
Malsha Gamage <malshagamage137@gmail.com>
2025-06-22 15:07:21 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/tmf-api': {
-        target: 'http://localhost:80',
+        target: 'https://geographic-address-management-api.onrender.com',
         changeOrigin: true,
         secure: false,
       }
